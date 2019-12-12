209e7145b1a3da0044bb95cd1094f0f17ad1ca4b
Hanna Mudrak <annmudrak@gmail.com>
2019-12-11 19:04:03 -0600

    del files vs mjs extension

--- src/bin/brain-games.js
+++
@@ -1,7 +0,0 @@
-#! /home/ann/.nvm/versions/node/v13.3.0/bin/node
-import { getName } from '../index.js';
-
-console.log('Welcome to the Brain Games!');
-const name = getName();
-const greeting = `Hello, ${name}`;
-console.log(greeting);
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import readlineSync from 'readline-sync';
-
-export const getName = () => {
-// export default () => {
-const name = readlineSync.question('May I have your name? ');
-    return name;
-};
-
