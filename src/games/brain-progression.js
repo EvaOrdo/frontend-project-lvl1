d1082bc2b4022f5f0637eec2a63e66161256674c
Hanna Mudrak <annmudrak@gmail.com>
2020-02-27 13:53:27 -0600

    align code

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -3,22 +3,22 @@ import makeGame from '../index.js';
 
 const sequenceLength = 10;
 const makeSequence = (startNum, diff, missingPosition) => {
-    let sequence = '';
-    for (let i = 0; i < sequenceLength; i += 1) {
+  let sequence = '';
+  for (let i = 0; i < sequenceLength; i += 1) {
     let el = startNum + i * diff;
     if (i === missingPosition) {
       el = '..';
     }
     sequence = `${sequence} ${el}`;
-    }
-    return sequence;
-  };
+  }
+  return sequence;
+};
 
 const getGameData = () => {
   const firstElement = getRandom(1, 20);
   const difference = getRandom(1, 10);
-  const missingElementPosition = getRandom(0, sequenceLength-1);
-  
+  const missingElementPosition = getRandom(0, sequenceLength - 1);
+
   const question = makeSequence(firstElement, difference, missingElementPosition);
   const rightAnswer = firstElement + missingElementPosition * difference;
 
